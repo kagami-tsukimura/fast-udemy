fd9a97c4dcb7e4d39856420778bf59a24b5fe9a1
Kagami <shimura.kyohei7617@gmail.com>
2024-06-10 21:27:09 +0900

    auto: dummy responses

--- routers/contact.py
+++ routers/contact.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import APIRouter
 
 import schemas.contact as contact_schema
@@ -8,9 +10,22 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get("/", response_model=list[contact_schema.Contact])
 async def get_contact_all():
-    pass
+    dummy_data = datetime.now()
+
+    return [
+        contact_schema.Contact(
+            id=1,
+            name="test",
+            email="test",
+            url="test@test.com",
+            gender=1,
+            message="test",
+            is_enabled=True,
+            created_at=dummy_data,
+        )
+    ]
 
 
 @router.post("/")
