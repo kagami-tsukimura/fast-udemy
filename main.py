5c426b25ba90ed7a7fd73ae21cfc921429910c35
Kagami <shimura.kyohei7617@gmail.com>
2024-06-10 19:41:37 +0900

    auto: main

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-from time import time
-
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    # localhost:3000からの操作のみ許可
-    allow_origins=["http://localhost:3000"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-# 処理時間計測ミドルウェア
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time()
-    response = await call_next(request)
-    # 処理の所要時間をレスポンスヘッダに記載
-    process_time = time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
-
-    return response
 
 
 @app.get("/")
