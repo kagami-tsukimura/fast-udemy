ac0017eb52ea48fe918156adeffcc7de5811e81b
Kagami <shimura.kyohei7617@gmail.com>
2024-06-10 19:47:56 +0900

    auto: main

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ async def example():
     起動確認としてHello Worldを返します。
     """
 
-    return {"message": "Hello World"}
+    return {"message": "Hello Worlds"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
