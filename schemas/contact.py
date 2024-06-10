385ba35774bf45774a7e141521576317e6e3d0ad
Kagami <shimura.kyohei7617@gmail.com>
2024-06-10 21:22:43 +0900

    auto: schemas

--- schemas/contact.py
+++ schemas/contact.py
@@ -1,3 +1,14 @@
-from typing import Optional
+from datetime import datetime
 
 from pydantic import BaseModel, Field
+
+
+class Contact(BaseModel):
+    id: int
+    name: str
+    email: str
+    url: str
+    gender: int
+    message: str
+    is_enabled: bool
+    created_at: datetime
