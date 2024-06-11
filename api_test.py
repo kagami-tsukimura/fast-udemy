3770226deecf06ae99b4219d11db25bbe17dbfa7
Kagami <shimura.kyohei7617@gmail.com>
2024-06-11 20:56:26 +0900

    auto: api

---
+++ api_test.py
@@ -0,0 +1,25 @@
+import json
+from datetime import datetime
+
+import requests
+
+
+def main():
+    url = "http://0.0.0.0:8001/contacts"
+    current_datetime = datetime.now().isoformat()
+    body = {
+        "id": 1,
+        "name": "test",
+        "email": "test@test.com",
+        "url": "https://example.com/",
+        "gender": 1,
+        "message": "test",
+        "created_at": current_datetime,
+    }
+
+    res = requests.post(url, json.dumps(body))
+    print(res.json())
+
+
+if __name__ == "__main__":
+    main()
