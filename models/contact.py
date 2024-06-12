86ee5a447a6922f3c802222baa66cdcf64041ecd
Kagami <shimura.kyohei7617@gmail.com>
2024-06-12 21:07:40 +0900

    auto: db

--- models/contact.py
+++ models/contact.py
@@ -16,12 +16,3 @@ class Contact(Base):
     message = Column(String(200), nullable=False)
     is_enabled = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.now())
-
-    def __init__(self, name, email, url, gender, message, is_enabled):
-        self.name = name
-        self.email = email
-        self.url = url
-        self.gender = gender
-        self.message = message
-        self.is_enabled = is_enabled
-        self.created_at = datetime.now()
