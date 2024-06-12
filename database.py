1e93479a6ef3668c607b28cb02664bce90229f2c
Kagami <shimura.kyohei7617@gmail.com>
2024-06-12 20:32:56 +0900

    auto: db

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DB_URL = "sqlite+aiosqlite:///fastapi-app.db"
+engine = create_async_engine(DB_URL, echo=True)
+Base = declarative_base()
+
+db_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
+)
