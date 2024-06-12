2adc739717295bf7e10466c11bb0eea788969f4c
Kagami <shimura.kyohei7617@gmail.com>
2024-06-12 22:14:34 +0900

    auto: schema

--- schemas/contact.py
+++ schemas/contact.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel, EmailStr, Field, HttpUrl
 
 
 class ContactBase(BaseModel):
-    id: int
     # 「...」 必須
     name: str = Field(..., min_length=2, max_length=50)
     email: EmailStr
@@ -12,4 +11,3 @@ class ContactBase(BaseModel):
     gender: int = Field(..., strict=True, ge=0, le=2)
     message: str = Field(..., max_length=200)
     is_enabled: bool = Field(default=False)
-    created_at: datetime
