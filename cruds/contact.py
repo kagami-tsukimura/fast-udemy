03759662e15a58e63f297a076de480fb8fddbfc0
Kagami <shimura.kyohei7617@gmail.com>
2024-06-13 19:05:02 +0900

    auto: create

--- cruds/contact.py
+++ cruds/contact.py
@@ -5,7 +5,7 @@ import schemas.contact as contact_schema
 
 
 async def create_contact(db: AsyncSession, contact: contact_schema.ContactCreate):
-    db_contact = contact_model.Contact(**contact.dict())
+    db_contact = contact.model_dump()
     db.add(db_contact)
     await db.commit()
     await db.refresh(db_contact)
