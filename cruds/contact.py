b342d9d45ce1d97eccc0a41205ab5fd34cdf2dca
Kagami <shimura.kyohei7617@gmail.com>
2024-06-13 19:04:30 +0900

    auto: create

---
+++ cruds/contact.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import models.contact as contact_model
+import schemas.contact as contact_schema
+
+
+async def create_contact(db: AsyncSession, contact: contact_schema.ContactCreate):
+    db_contact = contact_model.Contact(**contact.dict())
+    db.add(db_contact)
+    await db.commit()
+    await db.refresh(db_contact)
+    return db_contact
