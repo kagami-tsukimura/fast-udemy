7bb77da1d27477256c0ba820c6ac879ce5cdfb57
Kagami <shimura.kyohei7617@gmail.com>
2024-06-13 19:11:31 +0900

    auto: create

--- cruds/contact.py
+++ cruds/contact.py
@@ -7,6 +7,15 @@ import schemas.contact as contact_schema
 async def create_contact(
     db: AsyncSession, contact: contact_schema.ContactCreate
 ) -> contact_model.Contact:
+    """
+    Create DB Contact
+    Args:
+        db: AsyncSession
+        contact: contact_schema.ContactCreate
+    Returns:
+        contact_model.Contact
+    """
+
     db_contact = contact.model_dump()
     if db_contact["url"] is not None:
         db_contact["url"] = str(db_contact["url"])
